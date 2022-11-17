e0611d8b5eead54376c1c60ee6483a3a4eebd59e
Urban Vadzim <Herr.Wallace@protonmail.com>
2022-11-17 16:17:40 +0300

    add chapter 6 breadth-first search

---
+++ chapter6/breadthFirstSearch.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const graph = new Map();
+graph.set('you', ['alice', 'bob', 'claire']);
+graph.set('bob', ['anuj', 'peggy']);
+graph.set('alice', ['peggy']);
+graph.set('claire', ['thom', 'jonny']);
+graph.set('anuj', []);
+graph.set('peggy', []);
+graph.set('thom', []);
+graph.set('jonny', []);
+
+const search = (name) => {
+  let searched = [];
+  let search_queue = [...graph.get(name)];
+
+  while (search_queue) {
+    let person = search_queue.shift();
+
+    if (!searched.includes(person)) {
+      if (person[person.length - 1] === 'm') {
+        console.log(`${person} is a mango seller!`);
+        return;
+      } else {
+        search_queue.push(...graph.get(person));
+        searched.push(person);
+      }
+    }
+  }
+  return false;
+};
+
+// console.log(search('you'));
